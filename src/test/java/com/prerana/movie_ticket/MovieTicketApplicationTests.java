106a4bb0cfd8f29f10162cb09da5ac279adc53d6
preranahp <preranahp254@gmail.com>
2024-08-03 17:56:55 +0530

    springboot

---
+++ src/test/java/com/prerana/movie_ticket/MovieTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prerana.movie_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
